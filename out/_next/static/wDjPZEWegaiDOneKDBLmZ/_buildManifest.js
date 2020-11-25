9af1ee66d9b4796102e8c85af33198c02b5c303f
Ricardo Canelas <ricardocanelas@gmail.com>
2020-11-25 19:27:07 -0300

    chore: remove the 'out' folder

--- out/_next/static/wDjPZEWegaiDOneKDBLmZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-400f3101f0403f63ce2d.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-85b0ecfdd77778edbcba.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
